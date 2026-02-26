3bbdce4f2ab3cba5b7ad082938d53a1807ce4606
Ludwig_Bonaparte <yuvraj100706@gmail.com>
2026-02-27 00:52:12 +0530

    Unified the replace methods to one depending on labels per loop, defined closure fixation

--- Anonymizer.py
+++ Anonymizer.py
@@ -45,8 +45,32 @@ class Anonymizer:
         return text,masked_emails,masked_phones
     
 anonymizer=Anonymizer()
-master_text,masked_emails,masked_phones=anonymizer.mask(master_text)
-print(masked_emails)
-print(masked_phones)
-print(master_text)
+#master_text,masked_emails,masked_phones=anonymizer.mask(master_text)
+#print(masked_emails)
+#print(masked_phones)
+#print(master_text)
 
+class Anonymizer_advanced:
+    def __init__(self):
+        self.patterns={
+            "EMAIL":r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b',
+            "PHONE":r'\b[0-9]{10}\b'
+        }
+        self.labels={
+            "EMAIL":"[EMAIL]",
+            "PHONE":"[PHONE]"
+        }
+        
+    def mask(self,text):
+        for label,pattern in self.patterns.items():
+            '''def replace(match):
+                return self.labels[label] #we can't use this because label access only the value at the end of the loop i.e "PHONE"
+            text=re.sub(pattern,replace,text)''' #However this would still work but it's not always safe to rely on
+            def replace(match,label=label):
+                return self.labels[label] #we can use this because we are passing the label as a default argument to the function and thus it will access the correct label value for each pattern
+            text=re.sub(pattern,replace,text)
+        return text 
+    
+anonymizer_adv=Anonymizer_advanced()
+master_text=anonymizer_adv.mask(master_text)
+print(master_text)
